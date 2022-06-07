6a58b390f67c36d1f0871154f74c19b5af430c6e
chendelin1982 <27513732@qq.com>
2022-06-07 14:47:33 +0800

    created

--- config/node/service.sh
+++ config/node/service.sh
@@ -1,9 +1,45 @@
+#!/bin/bash
 
-#-------------------
+env  NODE_ROOT_PATH
+env  NODE_APP_NAME
 
-#if  vuepress/main.js exit
-node vuepress/main.js &
 
-#-------------------
+appPath="/home/node/app"
+installExpress(){
+    mkdir $appPath/express && cd $appPath/express
+    yarn add express
 
-sleep 500s
+    cat <<'EOF' > express/main.js
+    const express = require("express");
+    const app = express();
+    const hostname = "0.0.0.0";
+    const port = 3000;
+    app.get("/", (req, res) => {
+            res.send("Hello World");
+        });
+    app.listen(port, () => {
+            console.log(`Server running at http://${hostname}:${port}/`);
+        });
+    EOF
+    }
+
+if ! command -v pm2 &> /dev/null
+then
+    echo "Create sample and pm2 for your reference"
+    yarn global add pm2
+    installExpress
+    exit
+fi
+
+cd /home/node/app
+
+#---------Insert  you command to start Node application  Start----------
+
+if test -f vuepress/main.js
+then
+    pm2 vuepress/main.js
+fi
+
+#---------Insert  you command to start Node application  End----------
+
+tail -f /dev/null
