fa6c78e7c15b98bf8bec8d7790f8481407bc12bc
qiaofeng1227 <76487013@qq.com>
2022-06-08 14:15:15 +0800

    Update service.sh

--- config/node/service.sh
+++ config/node/service.sh
@@ -12,10 +12,10 @@ const hostname = "0.0.0.0";
 const port = 3000;
 app.get("/", (req, res) => {
        res.send("Hello World");
-    });
- app.listen(port, () => {
+});
+app.listen(port, () => {
        console.log(`Server running at http://${hostname}:${port}/`);
-    });
+});
 EOF
 }
 
