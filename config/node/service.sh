123d8362f09f695b75c921be785aee6fa51390d1
qiaofeng1227 <76487013@qq.com>
2022-06-08 12:07:39 +0800

    Update service.sh

--- config/node/service.sh
+++ config/node/service.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-env  NODE_ROOT_PATH
-env  NODE_APP_NAME
-
-
-appPath="/home/node/app"
-installExpress(){
-    mkdir $appPath/express && cd $appPath/express
+installApp(){
+   
+    mkdir -p $1/$2  && cd $1/$2
     yarn add express
 
-    cat <<'EOF' > express/main.js
-    const express = require("express");
-    const app = express();
-    const hostname = "0.0.0.0";
-    const port = 3000;
-    app.get("/", (req, res) => {
-            res.send("Hello World");
-        });
-    app.listen(port, () => {
-            console.log(`Server running at http://${hostname}:${port}/`);
-        });
-    EOF
-    }
+cat > $1/$2/main.js <<-EOF
+const express = require("express");
+const app = express();
+const hostname = "0.0.0.0";
+const port = 3000;
+app.get("/", (req, res) => {
+       res.send("Hello World");
+    });
+ app.listen(port, () => {
+       console.log(`Server running at http://${hostname}:${port}/`);
+    });
+EOF
+}
 
-if ! command -v pm2 &> /dev/null
-then
-    echo "Create sample and pm2 for your reference"
-    yarn global add pm2
-    installExpress
-    exit
+# install pm2 and setup runtime project
+echo "Create sample and pm2 for your reference"
+yarn global add pm2
+installExpress $NODE_ROOT_PATH $NODE_APP_NAME
+
+if [ $2  -eq "express"];then
+
+elif [ $2  -eq "xxx"];then
+
+else
+  echo "not support $2 now!"
 fi
 
-cd /home/node/app
-
-#---------Insert  you command to start Node application  Start----------
-
-if test -f vuepress/main.js
-then
-    pm2 vuepress/main.js
-fi
-
-#---------Insert  you command to start Node application  End----------
+cd $1
+pm2 $2/main.js
 
 tail -f /dev/null
