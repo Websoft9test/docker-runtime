d52488c20701af1927e743977aa1599ba0c2ab69
qiaofeng1227 <76487013@qq.com>
2022-06-20 14:23:18 +0800

    Update Dockerfile.php

--- src/Dockerfile.php
+++ src/Dockerfile.php
@@ -1,14 +1,12 @@
-FROM nginx:stable-alpine
+FROM php:8.1-fpm
 
 LABEL maintainer="help@websoft9.com"
 LABEL version="1.0.1"
-LABEL description="9panel"
+LABEL description="PHP runtime for 8.1"
 
-ENV RUNTIME_LANG="LAMP"
+ENV RUNTIME_LANG="PHP runtime"
 
-COPY  ./9panel /usr/share/nginx/html
-COPY ./entrypoint-9panel.sh /docker-entrypoint.d/
+# install os package
 
-RUN set -ex; \
-    ls /usr/share/nginx/html; \
-    chmod +x /docker-entrypoint.d/entrypoint-9panel.sh
+# install php module
+
