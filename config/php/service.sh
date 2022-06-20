65c963f7153b7b4313e86080d5e6c58e762edd0e
qiaofeng1227 <76487013@qq.com>
2022-06-20 16:50:25 +0800

    Update service.sh

--- config/php/service.sh
+++ config/php/service.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cp /opt/config/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
+
 # edit supervisord.conf
 # sed -i "s/autostart=*./autostart=false/g" /etc/supervisor/conf.d/supervisord.conf
 
