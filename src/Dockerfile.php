5b3ddf5ddb7f7f1e1f3f8312307bed2259287671
qiaofeng1227 <76487013@qq.com>
2022-06-20 16:15:14 +0800

    Update Dockerfile.php

--- src/Dockerfile.php
+++ src/Dockerfile.php
@@ -72,7 +72,22 @@ RUN	apt-get install -y --no-install-recommends \
 		pdo_mysql \
 		pdo_pgsql \
                 pgsql \
+                mysqli \
 		zip
+
+# install composer
+RUN     php -r "copy('https://getcomposer.org/installer', 'composer-setup.php')"; \
+        php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL"; \
+        php composer-setup.php; \
+        php -r "unlink('composer-setup.php')"; \
+        php composer.phar; \
+        mv composer.phar /usr/local/bin/composer
+
+# install Laravel,ThinkPHP,Symfony,Yii
+RUN     composer create-project laravel/laravel laravel-app; \
+        composer create-project topthink/think tp-app; \
+        composer create-project symfony/skeleton:"6.1.*" symfony_app; \
+        composer create-project --prefer-dist yiisoft/yii2-app-basic yii-app
 
 # create workdir
 RUN mkdir -p /data/apps
