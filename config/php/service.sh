358139786f14720543e514daa888257e2123647b
qiaofeng1227 <76487013@qq.com>
2022-06-21 08:40:01 +0800

    Update service.sh

--- config/php/service.sh
+++ config/php/service.sh
@@ -6,5 +6,8 @@ chmod +x /etc/supervisor/conf.d/supervisord.conf
 # edit supervisord.conf
 # sed -i "s/autostart=*./autostart=false/g" /etc/supervisor/conf.d/supervisord.conf
 
+# access app workdir
+cd $APP_NAME-app
+
 # start by supervisord
 /usr/bin/supervisord
