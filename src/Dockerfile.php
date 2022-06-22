ec8e8b55715cec9a483b1908826598356c71b61b
qiaofeng1227 <76487013@qq.com>
2022-06-22 11:06:28 +0800

    Update Dockerfile.php

--- src/Dockerfile.php
+++ src/Dockerfile.php
@@ -7,7 +7,7 @@ LABEL version="${PHP_VERSION}"
 LABEL description="PHP runtime for ${PHP_VERSION}"
 
 # install os common package or other image, such as drupal, wordpress,owncloud(https://github.com/docker-library)
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && apt-get install -y --no-install-recommends \
                 acl \
                 mosh \
                 curl \
@@ -34,6 +34,7 @@ RUN apt-get update && apt-get install -y \
                 chrony \
                 gnupg dirmngr \
 		ghostscript \
+		unixodbc-dev \
 		libfreetype6-dev \
 		libjpeg-dev \
 		libpng-dev \
@@ -44,6 +45,16 @@ RUN apt-get update && apt-get install -y \
 		libicu-dev \
 		libldap2-dev \
 		libmemcached-dev \
+		libsnmp-dev \
+		libtidy-dev \
+		libmcrypt-dev \
+		libgmp-dev \
+		libmagickwand-dev \
+		libmagickcore-dev \
+		libc-client-dev \
+		libkrb5-dev \
+		libc-client-dev \
+		libbz2-dev \
 		libxml2-dev
 
 # install php module for other image, such as drupal, wordpress,owncloud(https://github.com/docker-library) and role_php
@@ -65,7 +76,38 @@ RUN docker-php-ext-configure gd \
 		pdo_pgsql \
                 pgsql \
                 mysqli \
+                soap \
+                snmp \
+                tidy \
+                gmp \
+                mysqli \
+                bz2 \
 		zip
+
+# config and install odbc and imap
+RUN { \
+      echo '# https://github.com/docker-library/php/issues/103#issuecomment-271413933'; \
+      echo 'AC_DEFUN([PHP_ALWAYS_SHARED],[])dnl'; \
+      echo; \
+      cat /usr/src/php/ext/odbc/config.m4; \
+    } > temp.m4; \
+    mv temp.m4 /usr/src/php/ext/odbc/config.m4; \
+    docker-php-ext-configure odbc --with-unixODBC=shared,/usr; \
+    docker-php-ext-install odbc; \
+    docker-php-ext-configure imap --with-kerberos --with-imap-ssl; \
+    docker-php-ext-install imap
+
+# install pecl packge
+RUN pecl install mongodb; \
+    pecl install xmlrpc-1.0.0RC3; \
+    echo "no" | pecl install apcu; \
+    echo "no" | pecl install redis; \
+    echo "no" | pecl install memcached; \
+    echo "mcrypt" | pecl install mcrypt; \
+    echo "imagick" | pecl install imagick
+
+
+
 
 # set recommended PHP.ini settings
 # see https://secure.php.net/manual/en/opcache.installation.php
