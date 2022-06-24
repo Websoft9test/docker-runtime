352573db646211afbd19ab9f2fa2c5cd3ef5f492
qiaofeng1227 <76487013@qq.com>
2022-06-24 10:56:54 +0800

    Update service.sh

--- config/php/service.sh
+++ config/php/service.sh
@@ -3,9 +3,7 @@
 cp /opt/config/php/supervisord.conf /etc/supervisor/conf.d/supervisord.conf
 chmod +x /etc/supervisor/conf.d/supervisord.conf
 
-# edit supervisord.conf
-# sed -i "s/autostart=*./autostart=false/g" /etc/supervisor/conf.d/supervisord.conf
-echo $PHP_APP
-
 # start by supervisord
 /usr/bin/supervisord
+supervisorctl start $PHP_APP
+tail -f /dev/null
